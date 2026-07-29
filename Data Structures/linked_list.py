32911bf72b365b5a64817bd5858a96ea9e251a8c
mekanbaymyradov <baymyradov.mekanx@gmail.com>
2026-07-29 20:09:49 +0500

    Add new method to linked list

--- Data Structures/linked_list.py
+++ Data Structures/linked_list.py
@@ -1,85 +1,81 @@
-from typing import Any
+
 
 class Node:
-    def __init__(self, data: Any, next=None) -> None:
+    def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
+
 class LinkedList:
-    def __init__(self) -> None:
+    def __init__(self):
         self.head = None
 
-    def insert_at_beginning(self, data: Any) -> None:
+    def insert_at_beginning(self, data):
         node = Node(data, self.head)
-
         self.head = node
 
-    def insert_at_end(self, data: Any) -> None:
-        if not self.head:
-            self.head = Node(data)
-            return
-            
-        curr = self.head
-        while curr.next:
-            curr = curr.next
-
-        curr.next = Node(data)
-
-    def print(self) -> None:
+    def print(self):
         if self.head is None:
             print("Linked list is empty")
             return
 
-        curr = self.head
         ll_str = ""
+        curr = self.head
         while curr:
-            ll_str += f"{curr.data}->"
+            ll_str += str(curr.data) + "->"
             curr = curr.next
-            
+
         print(ll_str)
 
-    def insert_values(self, data_list: list) -> None:
-        self.head = None
-        for data in data_list:
-            self.insert_at_end(data)
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
 
-    def get_length(self) -> int:
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+
+        curr.next = Node(data, None)
+
+    def get_length(self):
         count = 0
         curr = self.head
-
         while curr:
-            count += 1
             curr = curr.next
+            count += 1
             
         return count
 
-    def remove_at(self, index: int) -> None:
-        if index < 0 or index >= self.get_length() or not self.head:
-            raise Exception("Invalid index")
+    def remove_at(self, index):
+        if index < 0 or self.head is None:
+            raise IndexError("Index out of range")
 
         if index == 0:
             self.head = self.head.next
             return
 
         curr = self.head
-        count = 0
-        while curr and curr.next:
-            if count == index - 1:
-                curr.next = curr.next.next
-                return
+        for _ in range(index - 1):
+            if curr.next is None:
+                raise IndexError("Index out of range")
             curr = curr.next
-            count += 1
-
-    def insert_at(self, index: int, data: Any) -> None:
-        if index < 0 or index > self.get_length():
-            raise Exception("Invalid index")
             
+        if curr.next is None:
+            raise IndexError("Index out of range")
+
+        curr.next = curr.next.next
+
+    def insert_at(self, index, data):
+        if index < 0:
+            raise IndexError("Index out of range")
+
         if index == 0:
             self.insert_at_beginning(data)
             return
-
+        
+        count = 0    
         curr = self.head
-        count = 0
         while curr:
             if count == index - 1:
                 node = Node(data, curr.next)
@@ -88,21 +84,35 @@ class LinkedList:
                 
             curr = curr.next
             count += 1
-        
-        
-        
+            
+        raise IndexError("Index out of range")
 
+    def insert_after_value(self, data_after, data_to_insert):
+        if self.head is None:
+            raise ValueError("Linked list is empty")
+            
+        curr = self.head
+
+        while curr:
+            if curr.data == data_after:
+                node = Node(data_to_insert, curr.next)
+                curr.next = node
+                return
+            curr = curr.next
+
+        raise ValueError("Given data not found")
+        
+        
 
 if __name__ == "__main__":
     ll = LinkedList()
-    ll.insert_values(["apple", "banana", "orange"])
+    ll.insert_at_beginning("apple")
+    ll.insert_at_beginning("orange")
+    ll.insert_at_end("banana")
     ll.print()
-    print("length of linked list:", ll.get_length())
+    print("length:", ll.get_length())
     ll.remove_at(1)
-    ll.insert_at(0, "mango")
-    ll.insert_at(3, "grapes")
-    ll.insert_at(3, "figs")
+    ll.insert_at(1, "grapes")
+    ll.insert_after_value("orange","mango")
+    ll.insert_after_value("mango","apple")
     ll.print()
-    
-            
-        
